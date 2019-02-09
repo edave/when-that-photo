2c670caa7240ee186362fb3b4e5155d75331e476
David Pitman <dave@kytheram.com>
2019-02-08 18:56:33 -0800

    Data loading and processing

--- images-dl.sh
+++ images-dl.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 while IFS=, read -r col1 col2
 do
-    gsutil -m cp -r $col2 /data/kaggle/met/images/$col1
+    FILE=/data/kaggle/met/images/$col1.jpg
+    if ! [ -f $FILE ]; then
+    gsutil -m cp -r $col2 $FILE
+    fi
 done < /data/kaggle/met/images.csv
